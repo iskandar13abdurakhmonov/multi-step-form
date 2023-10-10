6874b6160f7bbd5229ba16f569d61978fae359cb
Iskandar Abdurakhmonov <iskandar.13.aburahmonov@gmail.com>
2023-10-10 16:18:10 +0500

    dropped styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 function App() {
-    return <div className="App"></div>
+    return (
+        <div className="App">
+            <div className="wrapper">
+                <main className="main">
+                    <div className="main__container">multi step form</div>
+                </main>
+                <footer className="footer">
+                    <div className="footer__container">footer</div>
+                </footer>
+            </div>
+        </div>
+    )
 }
 
 export default App
