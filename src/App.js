d7593f8a7948def2c957c8c8885b77a23ad1ef0f
Iskandar Abdurakhmonov <iskandar.13.aburahmonov@gmail.com>
2023-10-11 19:15:16 +0500

    sidebarr active class added

--- src/App.js
+++ src/App.js
@@ -5,91 +5,30 @@ import Form from './components/Form/Form'
 import FormContainer from './components/FormContainer/FormContainer'
 import Sidebar from './components/Siderbar/Sidebar'
 
-const steps = [
-    {
-        stepId: 1,
-        stepNum: 'STEP 1',
-        stepText: 'YOUR INFO',
-        isActive: false,
-    },
-    {
-        stepId: 2,
-        stepNum: 'STEP 2',
-        stepText: 'SELECT PLAN',
-        isActive: false,
-    },
-    {
-        stepId: 3,
-        stepNum: 'STEP 3',
-        stepText: 'ADD-ONS',
-        isActive: false,
-    },
-    {
-        stepId: 4,
-        stepNum: 'STEP 4',
-        stepText: 'SUMMARY',
-        isActive: false,
-    },
-]
-
 function App() {
-    const [active, setActive] = useState(false)
-
-    const [activeForm, setActiveForm] = useState(1)
-    console.log(activeForm)
-
-    const handeNextClick = (id) => {
-        if (activeForm < 4) {
-            setActiveForm()
-        }
-    }
-
-    const handlePrevClick = () => {
-        if (activeForm > 1) {
-            setActiveForm((prev) => prev - 1)
-        }
-    }
-
-    const handleClick = () => {
-        setActive(steps.map((step) => (step.isActive = true)))
-    }
-
     return (
         <div className="App">
             <AppProvider>
-                <Router>
-                    <Routes>
-                        <Route
-                            path="/"
-                            element={
-                                <div className="wrapper">
-                                    <main className="main">
-                                        <div className="main__container containerMain">
-                                            <FormContainer>
-                                                <Sidebar
-                                                    active={active}
-                                                    steps={steps}
-                                                    activeForm={activeForm}
-                                                    onNextClick={handeNextClick}
-                                                    onPrevClick={
-                                                        handlePrevClick
-                                                    }
-                                                    onaHandleClick={handleClick}
-                                                />
-                                                <Form />
-                                            </FormContainer>
-                                        </div>
-                                    </main>
-                                    <footer className="footer">
-                                        <div className="footer__container">
-                                            footer
-                                        </div>
-                                    </footer>
-                                </div>
-                            }
-                        />
-                    </Routes>
-                </Router>
+                <div className="wrapper">
+                    <main className="main">
+                        <div className="main__container containerMain">
+                            <FormContainer>
+                                <Router>
+                                    <Sidebar />
+                                    <Routes>
+                                        <Route
+                                            path="/"
+                                            element={<Form />}
+                                        />
+                                    </Routes>
+                                </Router>
+                            </FormContainer>
+                        </div>
+                    </main>
+                    <footer className="footer">
+                        <div className="footer__container">footer</div>
+                    </footer>
+                </div>
             </AppProvider>
         </div>
     )
--- src/components/Siderbar/Sidebar.jsx
+++ src/components/Siderbar/Sidebar.jsx
@@ -1,29 +1,75 @@
 import React from 'react'
 import style from './Sidebar.module.css'
+import { useLocation } from 'react-router-dom'
 
-function Sidebar({ active, steps, activeForm, onNextClick, onPrevClick, onaHandleClick  }) {
+const steps = [
+    {
+        stepId: 1,
+        stepNum: 'STEP 1',
+        stepText: 'YOUR INFO',
+        isActive: false,
+    },
+    {
+        stepId: 2,
+        stepNum: 'STEP 2',
+        stepText: 'SELECT PLAN',
+        isActive: false,
+    },
+    {
+        stepId: 3,
+        stepNum: 'STEP 3',
+        stepText: 'ADD-ONS',
+        isActive: false,
+    },
+    {
+        stepId: 4,
+        stepNum: 'STEP 4',
+        stepText: 'SUMMARY',
+        isActive: false,
+    },
+]
+
+function Sidebar() {
+
+    const location = useLocation()
+
+    const getLinkClass = (path) => {
+        return (
+            'stepId ' + (path === location.pathname ? `${style.active}` : undefined)
+        )
+    }
 
     return (
         <div className={style.sidebar}>
             <ul className={style.stepList}>
-                {steps.map((step) => (
-                    <li
-                        className={style.stepItem}
-                        key={step.stepId}
-                    >
-                        <div className={`${style.stepId} ${active ? style.active : ''}`}>{step.stepId}</div>
-                        <div className={style.stepFlex}>
-                            <span className={style.stepNum}>
-                                {step.stepNum}
-                            </span>
-                            <p className={style.stepText}>{step.stepText}</p>
-                        </div>
-                    </li>
-                ))}
-                <button onClick={() => onaHandleClick()}>click</button>
-                <button onClick={() => onPrevClick(1)}>Go Back</button>
-                {activeForm}
-                <button onClick={() => onNextClick()}>Next Step</button>
+                <li className={style.stepItem}>
+                    <div className={`${style.stepId} ${getLinkClass('/')}`}>1</div>
+                    <div className={style.stepFlex}>
+                        <span className={style.stepNum}>STEP 1</span>
+                        <p className={style.stepText}>YOUR INFO</p>
+                    </div>
+                </li>
+                <li className={style.stepItem}>
+                    <div className={`${style.stepId} ${getLinkClass('/select-plan')}`}>2</div>
+                    <div className={style.stepFlex}>
+                        <span className={style.stepNum}>STEP 2</span>
+                        <p className={style.stepText}>SELECT PLAN</p>
+                    </div>
+                </li>
+                <li className={style.stepItem}>
+                    <div className={`${style.stepId} ${getLinkClass('/add-ons')}`}>3</div>
+                    <div className={style.stepFlex}>
+                        <span className={style.stepNum}>STEP 3</span>
+                        <p className={style.stepText}>ADD-ONS</p>
+                    </div>
+                </li>
+                <li className={style.stepItem}>
+                    <div className={`${style.stepId} ${getLinkClass('/summary')}`}>4</div>
+                    <div className={style.stepFlex}>
+                        <span className={style.stepNum}>STEP 4</span>
+                        <p className={style.stepText}>SUMMARY</p>
+                    </div>
+                </li>
             </ul>
         </div>
     )
